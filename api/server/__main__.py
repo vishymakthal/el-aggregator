0502467fbe8294d4a72b598b2680bf8f8d7ae7ba
vishymakthal <vishy787@gmail.com>
2020-03-25 23:08:04 -0400

    Added error handlers for 404 and 500 and updated resource paths

--- api/server/__main__.py
+++ api/server/__main__.py
@@ -1,21 +1,25 @@
 #!/usr/bin/evn python3
-# import connexion
 from flask import Flask
 from server import encoder
 from server.controllers import player_controller, team_controller
-# app = connexion.App(__name__, specification_dir='./swagger/')
-# app.app.json_encoder = encoder.JSONEncoder
-# app.add_api('api.yaml', arguments={'title': 'El Aggregator API'})
 
 app = Flask(__name__)
 
-@app.route("/api/v1/player/<player_id>")
+@app.route("/api/v1/players/<player_id>")
 def get_player(player_id):
     return player_controller.get_player_by_id(player_id)
 
-@app.route("/api/v1/team/<team_id>")
+@app.route("/api/v1/teams/<team_id>")
 def get_team(team_id):
     return team_controller.get_team_by_id(team_id)
+
+@app.errorhandler(404)
+def resource_not_found(error):
+    return 'resource not found', 404 
+
+@app.errorhandler(500)
+def handle_500():
+    return 'internal server error', 500
 
 def main():
     app.run(port=8080)
